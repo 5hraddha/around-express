9de6142350df0971059541edec4f2c49ccbcf7be
Shraddha <mailmeatshraddha@gmail.com>
2022-01-06 17:29:11 -0500

    feat: add user router

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use('/users', userRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
