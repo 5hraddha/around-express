3974c374c11e1298ef310eb261151914c469f135
Shraddha <mailmeatshraddha@gmail.com>
2022-01-07 11:22:10 -0500

    feat: add card router for /cards api endpoint

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
