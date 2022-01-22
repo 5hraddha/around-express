c08b303f57476ebad7d1139bcaf3e4ff2914e4af
Shraddha <mailmeatshraddha@gmail.com>
2022-01-21 22:40:29 -0500

    feat: add user schema and model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const validateUrlRegex = /^(https?:\/\/)[w{3}]*\.?[a-z0-9\._\-~:\/\?%#\[\]@!$&'\(\)\*\+\,;=]+#?$/gmi;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: [true, 'User information is required.'],
+    trim: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: [true, 'A link to the user avatar is required'],
+    validate: {
+      validator: (url) => validateUrlRegex.test(url),
+      message: 'The entered link to the avatar is badly formed or contains invalid characters',
+    },
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
