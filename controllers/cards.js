d52de56951aae53b0a3bb63f17fba4727a431e6b
Shraddha <mailmeatshraddha@gmail.com>
2022-01-22 19:45:53 -0500

    feat: modify GET cards route handlers to interact with MongoDB

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,26 +2,33 @@
  * Define all the route handlers related to cards on `/cards` API endpoint.
  * @module controllers/cards
  */
-const path = require('path');
-const readFile = require('../utils/readFile');
-
-const cardsFilePath = path.join(__dirname, '..', 'data', 'cards.json');
+const Card = require('../models/card');
 
 /**
  * Router handler for GET request on `/cards` API endpoint to get all the cards.
  * @param {Object} req - The request object
  * @param {Object} res - The response object.
  * @return {object} `200` - success response with data - application/json.
+ * @return {object} `404` - The server can not find the requested resource.
  * @return {object} `500` - Internal server error response.
  */
 const getCards = (req, res) => {
-  readFile(cardsFilePath, res)
+  Card.find({})
+    .orFail()
     .then((cards) => res
       .status(200)
       .send(cards))
-    .catch(() => res
-      .status(500)
-      .send({ message: 'An error has occurred on the server' }));
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res
+          .status(404)
+          .send({ message: `${err.name} - Cards not found` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `${err.name} - An error has occurred on the server` });
+      }
+    });
 };
 
 module.exports = {
